8fdc72f9a599b6cc85b4a72ee91e424e808b2185
mpascucci <marpas.paris@gmail.com>
2022-02-11 08:21:04 +0100

    increment version micro

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.txt", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='multipagetiff',
-                 version='3.0.2',
+                 version='3.0.3',
                  description='open multipage tiff stacks and create z-projections',
                  author='Marco Pascucci',
                  long_description=long_description,
