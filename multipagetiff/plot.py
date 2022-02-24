f84f18924dcd8d99a262840896632ac311c1ce21
mpascucci <marpas.paris@gmail.com>
2022-02-24 19:38:42 +0100

    update header disclaimer in files

--- multipagetiff/plot.py
+++ multipagetiff/plot.py
@@ -1,3 +1,30 @@
+"""
+
+MULTIPAGETIFF
+
+tools for multipage tiff images manipulation
+
+author: Marco Pascucci
+copyright: 2018
+
+
+This file is part of MULTIPAGETIFF.
+
+MULTIPAGETIFF is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MULTIPAGETIFF is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MULTIPAGETIFF.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
 import matplotlib.gridspec as _gridspec
 from .config import config as _config
 from matplotlib import pyplot as _plt
