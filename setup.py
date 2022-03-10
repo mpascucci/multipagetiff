f336e9161166e561339ffcd6178a541195a50190
mpascucci <marpas.paris@gmail.com>
2022-03-10 13:26:14 +0100

    setup auto find package

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.txt", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='multipagetiff',
-                 version='3.0.6',
+                 version='3.0.7',
                  description='open multipage tiff stacks and create z-projections',
                  author='Marco Pascucci',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
                  author_email='marpas.paris@gmail.com',
                  url='https://github.com/mpascucci/multipagetiff',
-                 packages=['multipagetiff'],
+                 packages=setuptools.find_packages(),
                  install_requires=['numpy', 'matplotlib', 'tqdm'],
                  classifiers=[
                      "Programming Language :: Python",
